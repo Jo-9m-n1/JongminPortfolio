f5397e0ea9336088e7b133006cde89a86fcfa29b
Jo-9m-n1 <leejongmin1092@gmail.com>
2026-02-15 21:27:16 -0500

    Added Chemically bonding.

--- api/index.py
+++ api/index.py
@@ -8,14 +8,26 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 PROJECTS = [
     {
+        'id': 'chemically-bonding',
+        'title': 'Chemically Bonding',
+        'category': 'School Project 2026',
+        'tech': ['Python', 'JavaScript', 'Gemini API'],
+        'description': 'Currently developing the website',
+        'full_story': 'Detailed analysis forthcoming.',
+        'full_tech': ['Python', 'JavaScript', 'Gemini API'],
+        'troubles': 'Currently developing the website',
+        'collaborators': ['James', 'Alexander'],
+        'images': []
+    },
+    {
         'id': 'personal-website',
         'title': 'Personal Website',
         'category': 'Personal Project 2026',
         'tech': ['Python', 'JavaScript'],
         'description': 'Developed a responsive personal portfolio website using Flask and Python for backend architecture',
-        'full_story': 'EMPTY',
+        'full_story': 'Detailed analysis forthcoming.',
         'full_tech': ['Python', 'JavaScript'],
-        'troubles': 'EMPTY',
+        'troubles': 'Detailed analysis forthcoming.',
         'images': ["/static/MyPersonalWebsite1.png", 
                    "/static/MyPersonalWebsite2.png"]
     },
@@ -26,15 +38,15 @@ PROJECTS = [
         'category': 'Hackathon 2026',
         'tech': ['Python', 'JavaScript', 'Twilio API', 'Gemini API'],
         'description': 'Developed at a hackathon, Dr. Bob uses Twilio to automate patient intake and history tracking, ensuring medical data is organized for doctors.',
-        'full_story': 'EMPTY',
+        'full_story': 'Detailed analysis forthcoming.',
         'full_tech': ['Python', 'JavaScript', 'SQLite', 'SQLAlchemy', 'Twilio API', 'Gemini API', 'Web Speech API', 'Geolocation API', 'Werkzeug Security'],
         'troubles': 'The most complex technical achievement was synchronizing the Twilio recording with Gemini API. I had to architect an asynchronous pipeline that captured live patient audio, retrieved the remote recording via webhooks, and processed the binary data for AI analysis, while maintaining a low-latency user experience essential for medical triage.',
         'collaborators': ['Oliver'],
-        'images': ["/static/DrBob2.png", 
+        'images': ["/static/DrBob5.png", 
                    "/static/DrBob1.png", 
                    "/static/DrBob3.png",
                    "/static/DrBob4.png", 
-                   "/static/DrBob5.png", 
+                   "/static/DrBob2.png", 
                    "/static/DrBob6.png", 
                    "/static/DrBob8.png",
                    "/static/DrBob9.png",
@@ -42,11 +54,11 @@ PROJECTS = [
     },
     {
         'id': 'j-score',
-        'title': 'J-Score Calculator',
+        'title': 'J-Score',
         'category': 'School Project 2025',
-        'description': 'A precision tool for calculating academic standing (r-score) using standard deviation.',
+        'description': 'A precision tool for calculating academic standing (r-score) using the standard deviation.',
         'tech': ['Python', 'JavaScript', 'CSV'],
-        'full_story': 'James and I developed the J-Score as the final project for my third-semester programming class at Dawson College, it was the first large-scale collaborative web application I built with a teammate. While we were both familiar with the basics of Git, this project forced us to move from individual workflows to a synchronized development environment. This important skill helped me make application as a team and win few hackathons.',
+        'full_story': 'James and I developed the J-Score as the final project for our third-semester programming class at Dawson College, it was the first large-scale collaborative web application I built with a teammate. While we were both familiar with the basics of Git, this project forced us to move from individual workflows to a synchronized development environment. This important skill helped me make application as a team and win few hackathons.',
         'full_tech': ['Python', 'JavaScript', 'CSV'],
         'troubles': "Initially, the Flask application couldn't distinguish which user was requesting a deletion; it would simply remove the top line of the CSV database, regardless of who it belonged to. This created a major data integrity issue. To solve this, I researched how to pass contextual data through the frontend without cluttering the UI. I implemented hidden HTML inputs to bind specific user metadata to the request. This allowed the backend to verify the user’s identity and target the correct row in the CSV file, ensuring that users could only modify their own data. This challenge taught me the vital importance of state management and request context in web applications.",
         'collaborators': ['James'],
@@ -246,7 +258,7 @@ def achievements():
             'event': 'GIO Engineering',
             'date': 'June 2025 - August 2025',
             'desc': 'I developed CAD files tailored for architectural projects which can serve as the foundational blueprints for design and construction phases.',
-            'category': 'stem math job'
+            'category': 'stem math job software'
         },
         {
             'title': "Dean's List",
