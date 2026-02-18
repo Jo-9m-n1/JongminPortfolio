bc08616a7fb70070e57dc9d6772c6412e06f2afd
Jo-9m-n1 <leejongmin1092@gmail.com>
2026-02-18 15:45:18 -0500

    Added Apple style CSS

--- api/index.py
+++ api/index.py
@@ -147,7 +147,7 @@ def achievements():
             'title': 'Participated: Dialogue Employees Hackathon',
             'event': 'Dialogue Health Technologies Inc',
             'date': '2026. 02. 12. - 2026. 02. 13.',
-            'desc': 'Got invited to their internal hackathon and integrated a skin analysis feature into the Dialogue application, enabling users to receive automated health assessments.',
+            'desc': 'Got invited to their internal hackathon and integrated a skin analysis feature into the Dialogue application, enabling users to receive automated health assessments using Skinive API.',
             'category': 'software health canada stem'
         },
         {
