03cdda3886f6326c46d96da794195a67423bd30a
Jo-9m-n1 <leejongmin1092@gmail.com>
2026-03-12 19:45:41 -0400

    Added time for Mila hackathon.

--- static/script.js
+++ static/script.js
@@ -1082,9 +1082,9 @@ May 2025 | Dawson Robotics Hackathon <i class="fa-solid fa-trophy"></i>
 `;
             } else if (args[1] === 'education.txt') {
                 outputArea.innerHTML += 
-`2019 - 2024               | Rosemount High School
+`2026 (Est.) - 2029 (Est.) | McGill University
 2024 - 2026 (Est.)        | Dawson College
-2026 (Est.) - 2029 (Est.) | McGill University
+2019 - 2024               | Rosemount High School
 `  
             } else if (args[1] === 'projects.txt') {
                 outputArea.innerHTML += 
